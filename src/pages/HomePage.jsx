8bca66ba95c943d710d1c54e740a7c21685e688f
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-22 17:41:52 +0300

    home page html template

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import "./HomePage.css";
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home-page">
+      <header>
+        <h1>TEKNOLOJİK YEMEKLER</h1>
+      </header>
+      <section>
+        <p>KOD ACIKTIRIR</p>
+        <p>PIZZA, DOYURUR</p>
+        <button>ACIKTIM</button>
+      </section>
     </div>
   );
 }
