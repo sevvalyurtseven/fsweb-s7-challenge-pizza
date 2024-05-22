1ee985d720377051c578464979be2be78a14713b
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-22 16:49:51 +0300

    page template

---
+++ src/pages/OrderPizzaPage.jsx
@@ -0,0 +1,9 @@
+function OrderPizzaPage() {
+  return (
+    <div>
+      <h1>Order Pizza Page</h1>
+    </div>
+  );
+}
+
+export default OrderPizzaPage;
---
+++ src/pages/SuccessPage.jsx
@@ -0,0 +1,9 @@
+function SuccessPage() {
+  return (
+    <div>
+      <h1>Success Page</h1>
+    </div>
+  );
+}
+
+export default SuccessPage;
